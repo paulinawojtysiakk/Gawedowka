b06f0fd0e50ed52952123aea42b84792d486cef2
paulinawojtysiakk <paulina.wwojtysiak@gmail.com>
2023-10-26 22:09:18 +0200

    atoms created

---
+++ src/components/atoms/TextField/Validation/validation.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+export default function ValidationTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          error
+          id="filled-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="filled"
+        />
+        <TextField
+          error
+          id="filled-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+          variant="filled"
+        />
+      </div>
+    </Box>
+  );
+}
---
+++ src/components/atoms/Typography/typography.js
@@ -0,0 +1,8 @@
+import Typography from "@mui/material/Typography";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+<Typography variant="h1">Heading 1</Typography>;
